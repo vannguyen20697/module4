1102c2704d0c1961846b7576514124e3e337c5b5
vannguyen20697 <110239577+vannguyen20697@users.noreply.github.com>
2023-03-19 00:57:12 +0700

    Add files via upload

---
+++ ss3_Thymeleaf/thuc_hanh/src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,48 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.Customer;
+import com.codegym.service.CustomerService;
+import com.codegym.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Customer> customerList = customerService.findAll();
+        model.addAttribute("customers", customerList);
+        return "/index";
+    }
+    @GetMapping("/create")
+    //Giá trị trong @GetMapping tương ứng đường link của button "Add new customer".
+    public String create(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "/create";
+    }
+    @PostMapping("/save")
+    public String save(Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customer";
+    }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Customer customer) {
+        customerService.update(customer.getId(), customer);
+        return "redirect:/customer";
+    }
+}
